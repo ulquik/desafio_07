bb652d7aba231e6059628016a944b5ee7b2a164e
ulquik <rodrigolinsabareta2@gmail.com>
2021-09-25 19:12:33 -0300

    Adicionar arquivos de projeto.

---
+++ desafio 07/Conta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace desafio_07
+{
+    class Conta
+    {
+        public String titular;
+        public int numero;
+        public double saldo;
+
+
+        internal void SacarValor(double quantidade)
+        {
+            double novoSaldo = this.saldo - quantidade;
+            this.saldo = novoSaldo;
+        }
+
+        internal void DepositarValor(double quantidade)
+        {
+            this.saldo += quantidade;
+        }
+    }
+
+}
+
+
---
+++ desafio 07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace desafio_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta();
+
+            minhaConta.titular = "Rodrigo";
+            minhaConta.numero = 123456789;
+            minhaConta.saldo = 350;
+
+            minhaConta.SacarValor(65);
+            minhaConta.DepositarValor(98);
+
+            Console.WriteLine($"{minhaConta.titular} conta {minhaConta.numero} possui um saldo de: {minhaConta.saldo} reais");
+            
+        }
+    }
+}
